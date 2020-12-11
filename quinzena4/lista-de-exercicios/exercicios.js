7306f9db69a35e8620161a6c5ba97fffd305d88d
Pereira-Araujo <araujo_ir@yahoo.com>
2020-12-11 20:32:25 -0300

    exercício 2  :page_with_curl:

--- quinzena4/lista-de-exercicios/exercicios.js
+++ quinzena4/lista-de-exercicios/exercicios.js
@@ -3,5 +3,13 @@ Exercícios de interpretação de código
 */
 
 //1
-//A função irá à partir do "input" da const cotação(que pede a cotação do dolar), multiplicar o valor inserido pelo valor seguinte, determinado como valorEmDolar. 
+//A função irá à partir do "input" da const cotação(que pede a cotação do dolar), multiplicar o valor inserido pelo valor seguinte,
+// determinado como valorEmDolar. 
 //No caso em si, ele irá imprimir no console o valor da cotação do dolar multiplicado por 100;
+
+//2
+// A função, de acordo com cada opção do parametro tipoDeInvestimento está definindo o valor a ser multiplicado, caso a caso.
+//Fora da função, são declaradas duas variáveis, 
+//uma que chama a função com a opção "ações" e um valor de 150, tendo seu valor impresso no console como 165(150 x 1.1)  
+ //e outra que chama a função com a opção "tesouro direto" e um valor de 200,
+//e terá impresso no console: TIPO  DE INVESTIMENTO INFORMADO INCORRETO!
