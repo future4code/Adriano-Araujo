6b789b1b9615347295e21bc4291cf56dafd94f02
Pereira-Araujo <araujo_ir@yahoo.com>
2020-12-11 20:18:25 -0300

    exercício 1 :page_with_curl:

---
+++ quinzena4/lista-de-exercicios/exercicios.js
@@ -0,0 +1,7 @@
+/*
+Exercícios de interpretação de código
+*/
+
+//1
+//A função irá à partir do "input" da const cotação(que pede a cotação do dolar), multiplicar o valor inserido pelo valor seguinte, determinado como valorEmDolar. 
+//No caso em si, ele irá imprimir no console o valor da cotação do dolar multiplicado por 100;
