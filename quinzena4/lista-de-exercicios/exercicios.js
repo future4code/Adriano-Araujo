f745f5e28097172282f3e6f63ebe923bd35079a6
Pereira-Araujo <araujo_ir@yahoo.com>
2020-12-12 23:27:04 -0300

    lógica 3 :bulb:

--- quinzena4/lista-de-exercicios/exercicios.js
+++ quinzena4/lista-de-exercicios/exercicios.js
@@ -2,8 +2,8 @@
 EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO*/
 
 //1
-//A função irá à partir do "input" da const cotação(que pede a cotação do dolar), multiplicar o valor inserido pelo valor seguinte,
-// determinado como valorEmDolar. 
+//A função irá à partir do "input" da const cotação(que pede a cotação do dolar),
+// multiplicar o valor inserido pelo valor seguinte, determinado como valorEmDolar. 
 //No caso em si, ele irá imprimir no console o valor da cotação do dolar multiplicado por 100;
 
 //2
@@ -23,8 +23,8 @@ EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO*/
 //4
 // Se o "numero" for menor do que o "numero1" o valor de "numero1" será igual ao valor de "numero", no caso do array(o "numeros")
 // esse valor será igual ao -10(o menor numero do array)
-// Agora, se o "numero" for maior do que o "numero2", o "numero2" será igual ao "numero",  no caso do array(o "numeros"), esse valor
-// será igual a 1590(o maior numero do array)
+// Agora, se o "numero" for maior do que o "numero2", o "numero2" será igual ao "numero",  no caso do array(o "numeros"),
+ //esse valor será igual a 1590(o maior numero do array)
 
 
 /*
@@ -34,7 +34,7 @@ LÓGICA DE PROGRAMAÇÃO
 //1
 // Com for, for of, e forEach
 
-//array a ser precorrido
+// array a ser precorrido
 let array = [2, 4, 6, 8, 9, 10, 11, 14, 18]
 
 let index = 0 // programa que o percorre
@@ -49,4 +49,12 @@ let index = 0 // programa que o percorre
  // d. true
  // e. true
 
+ //3
+ // Não o código não funciona, ele imprimirá infinitos zeros(i*2, sendo i=0), acho que a melhor forma seria usar
+ // o "for" ao inves do "while", e um "<" menor quê, do que um "<=" menor ou igual a quê
+ const quantidadeNumerosPares = 8
+ let i = 0
+ for(i; i < quantidadeNumerosPares; i++) {
+     console.log(i*2)
+ }
  
