1e8d0c8ce5217b92c21874ee07ffa3269a847277
Pereira-Araujo <araujo_ir@yahoo.com>
2020-12-18 21:59:21 -0300

    Transformação dos botões em componentes de links

--- quinzena5/labedin/src/components/ImagemButton/ImagemButton.js
+++ quinzena5/labedin/src/components/ImagemButton/ImagemButton.js
@@ -3,7 +3,7 @@ import './ImagemButton.css'
 
 function ImagemButton(props) {
     return (
-        <a href={props.endereco} className="image-button-container">
+        <a href={props.endereco} target="_blank" className="image-button-container">
             <img src={ props.imagem }/>
             <p>{ props.texto }</p>
             
