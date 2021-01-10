2daf0ee0036c83fbd408b62d0c524a04f07d4bf3
Pereira-Araujo <araujo_ir@yahoo.com>
2021-01-10 11:08:26 -0300

    checkpoints 1 e 2

---
+++ quinzena5/instaLab-lista/insta-four/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import './SecaoComentario.css'
+
+export class SecaoComentario extends Component {
+	state = {
+      comentario:' ',
+	}
+
+	onChangeComentario =(event) => {
+		this.setState ({comentario: event.target.value})
+
+	}
+
+	
+
+	render() {
+
+		console.log(this.state.comentario)
+
+		return <div className={'comment-container'}>
+			<input
+				className={'input-comentario'}
+				placeholder={'Comentário'}
+				value={this.onClickComentario}
+				onChange={this.onChangeComentario}
+			/>
+			<button onClick={this.props.aoEnviar}>Enviar</button>
+			
+
+		</div>
+
+	}
+
+}
+
