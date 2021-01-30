3ffc56f1a95a327ca512c2b60a50b948f96e212a
Pereira-Araujo <araujo_ir@yahoo.com>
2021-01-30 15:10:10 -0300

    finalização :)

--- quinzena7/labe-login/src/Componentes/Cadastrar/Cadastrar.js
+++ quinzena7/labe-login/src/Componentes/Cadastrar/Cadastrar.js
@@ -8,20 +8,49 @@ const Container = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction:column;
+  flex-direction: column;
+  background: linear-gradient(
+    90deg,
+    hsla(40, 63%, 85%, 1) 0%,
+    hsla(22, 94%, 79%, 1) 100%
+  );
 `;
-
 const Cadastro = styled.section`
   width: 12rem;
-  height: 12rem;
+  height: 14rem;
   border-radius: 2%;
+  background: white;
+  box-shadow: -4px 11px 5px -1px rgba(0, 0, 0, 0.54);
+`;
+
+const UsuariosContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 40vw;
+  height: 40vh;
 `;
 
 const Lista = styled.p`
-border-bottom:solid black 1px;
-`
+  display: flex;
+  align-items: center;
+  margin-left: 1rem;
+  background: white;
+  padding: 0.5rem;
+  height: 1rem;
+  border-radius: 3%;
+  box-shadow: -4px 11px 5px -1px rgba(0, 0, 0, 0.54);
+`;
 
+const BotaoDeletar = styled.button`
+  background: white;
+  border: none;
+  margin-left: 1rem;
 
+  &:hover {
+    filter: invert(100%);
+  }
+`;
 
 class Cadastrar extends React.Component {
   state = {
@@ -36,8 +65,6 @@ class Cadastrar extends React.Component {
   onChangeEmailUsuario = (evento) => {
     this.setState({ emailUsuario: evento.target.value });
   };
-
-
 
   //MOSTRA
 
@@ -55,7 +82,25 @@ class Cadastrar extends React.Component {
       .catch(() => alert("ok"));
   };
 
-  // CRIA
+  deletaUsuario = (id) => {
+    axios
+      .delete(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
+        {
+          headers: {
+            Authorization: "araujo-muyembe",
+          },
+        }
+      )
+      .then(() => {
+        alert("Usuário deletado!");
+        this.usuarios();
+      })
+      .catch((erro) => {
+        alert(erro.message);
+      });
+  };
+
   criaUsuario = () => {
     const body = {
       name: this.state.nomeUsuario,
@@ -76,18 +121,21 @@ class Cadastrar extends React.Component {
         alert("Cadastro feito com sucesso!");
         this.setState({ nomeUsuario: "", emailUsuario: "" });
       })
-      .catch((erro) => {
-        console.log("Usuário não criado :( ");
+      .catch((error) => {
+        alert("Usuário não criado :( ");
       });
   };
 
-  componentDidMount() {
-    this.criaUsuario();
-  }
-
   render() {
     let listaCadastrados = this.state.lista.map((user) => {
-      return <Lista key={user.id}>{user.name}</Lista>;
+      return (
+        <Lista key={user.id}>
+          {user.name}
+          <BotaoDeletar onClick={() => this.deletaUsuario(user.id)}>
+            X
+          </BotaoDeletar>
+        </Lista>
+      );
     });
 
     return (
@@ -106,9 +154,8 @@ class Cadastrar extends React.Component {
           <button onClick={this.criaUsuario}>criar</button>
           <button onClick={this.usuarios}>lista</button>
         </Cadastro>
-         
-         <div>{listaCadastrados}</div>
-          
+
+        <UsuariosContainer>{listaCadastrados}</UsuariosContainer>
       </Container>
     );
   }
